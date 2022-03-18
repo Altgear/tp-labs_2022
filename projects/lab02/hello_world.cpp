74682e7beeadc9de7e3264b2e1c48dd522a485ce
Altgear <tagir337@gmail.com>
2022-03-18 14:03:14 +0300

    changed Hello_world.cpp

--- projects/lab02/hello_world.cpp
+++ projects/lab02/hello_world.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-	cout << "Hello world!" << endl;
+	string username;
+	cin >> username;
+	cout << "Hello world from " << username << "!" << endl;
 }
